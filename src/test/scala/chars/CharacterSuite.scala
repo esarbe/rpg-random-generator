de526d3b61c530336f781daac72029c0f56e0ebe
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-11-25 22:39:15 +0100

    implement Random monad with cats

--- src/test/scala/chars/CharacterSuite.scala
+++ src/test/scala/chars/CharacterSuite.scala
@@ -1,14 +1,30 @@
 package chars
 
-import cats.Functor
+import cats._
 import io.rbricks.itemized.{IndexedItemized, Itemized, ItemizedCodec, ItemizedIndex}
 import io.rbricks.itemized.annotation.{enum, indexedEnum}
 import org.scalatest.FunSuite
 
-
 class CharacterSuite extends FunSuite {
 
   type Random[T] = (Long) => (Long, T)
+
+  implicit def randomMonad: Monad[Random] = new Monad[Random] {
+    override def map[A, B](fa: Random[A])(f: (A) => B): Random[B] = {
+      flatMap(fa)( a => pure(f(a)) )
+    }
+
+    override def flatMap[A, B](fa: Random[A])(f: (A) => Random[B]): Random[B] = { seed =>
+      val (nextSeed, value) = fa(seed)
+      f(value)(nextSeed)
+    }
+
+    override def tailRecM[A, B](a: A)(f: (A) => Random[Either[A, B]]): Random[B] = defaultTailRecM(a)(f)
+
+    override def pure[A](value: A): Random[A] = { seed =>
+      (seed, value)
+    }
+  }
 
   def next(bits: Int): Random[Int] = { seed =>
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1)
@@ -18,22 +34,6 @@ class CharacterSuite extends FunSuite {
   }
 
   def randomInt: Random[Int] = next(32)
-
-  def always[T](value: T): Random[T] = { seed =>
-    (seed, value)
-  }
-
-  implicit class RandomOps[A](run: Random[A]) {
-
-    def map[B](f: A => B): Random[B] = {
-      flatMap( a => always(f(a)) )
-    }
-
-    def flatMap[B](f: A => Random[B]): Random[B]= { seed =>
-      val (nextSeed, value) = run(seed)
-      f(value)(nextSeed)
-    }
-  }
 
   @indexedEnum trait Color {
     type Index = Int
