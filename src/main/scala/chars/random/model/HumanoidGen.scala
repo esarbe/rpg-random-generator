dd3883ad3616b5eef989f825278fa5a05bbd7b12
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-12-15 18:54:21 +0100

    start using weighted enums

--- src/main/scala/chars/random/model/HumanoidGen.scala
+++ src/main/scala/chars/random/model/HumanoidGen.scala
@@ -9,20 +9,36 @@ object HumanoidGen extends Random[Humanoid] {
   import chars.random.implicits._
 
   val randomValueCategory = randomEnum[Humanoid.CategorizedValue]
-  val randomColor = randomEnum[Humanoid.Face.Eyes.Eye.Color]
-  val randomAge = randomEnum[Humanoid.Age]
+
+  val randomEyeColor = randomEnumWithWeights[Humanoid.Face.Eyes.Eye.Color] {
+    case Humanoid.Face.Eyes.Eye.Color.Brown => 2
+    case Humanoid.Face.Eyes.Eye.Color.Blue => 1
+    case Humanoid.Face.Eyes.Eye.Color.Hazel => 0.5
+    case Humanoid.Face.Eyes.Eye.Color.Green => 0.2
+  }
+
+  val randomAge = randomEnumWithWeights[Humanoid.Age]{
+    case Humanoid.Age.Young => 2
+    case Humanoid.Age.Middleaged => 3
+    case Humanoid.Age.Old => 1
+    case Humanoid.Age.Ancient => 0.5
+  }
+
   val randomBodyHeight = randomValueCategory.map(Humanoid.Body.Height)
   val randomBodyWeight = randomValueCategory.map(Humanoid.Body.Weight)
   val randomAthleticism = randomValueCategory.map(Humanoid.Body.Athleticism)
   val randomBody = (randomBodyHeight |@| randomBodyWeight |@| randomAthleticism).map(Humanoid.Body.apply)
   val randomFaceShape = randomEnum[Humanoid.Face.Shape]
   val randomEyeShape = randomEnum[Humanoid.Face.Eyes.Eye.Shape]
-  val randomEyeColor = randomEnum[Humanoid.Face.Eyes.Eye.Color]
 
   val randomEyes = (randomEyeColor |@| randomEyeShape).map(Humanoid.Face.Eyes.Eye.apply)
   val randomFace = (randomFaceShape |@| randomEyes).map(Humanoid.Face.apply)
   val randomHead = (randomFace |@| randomValueCategory).map(Humanoid.Head.apply)
-  val randomSex = randomEnum[Humanoid.Sex]
+  val randomSex = randomEnumWithWeights[Humanoid.Sex] {
+    case Humanoid.Sex.Male => 1.2
+    case Humanoid.Sex.Female => 0.8
+    case Humanoid.Sex.Indeterminate => 0.1
+  }
 
   val randomRace = randomEnum[Humanoid.Race]
 
