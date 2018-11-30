36f2a78d37b3d150261cac9a326e2ca70dd5972b
Raphael Bosshard <raphael.bosshard@gmail.com>
2018-11-30 22:23:55 +0100

    add helper for random

--- src/main/scala/chars/random/package.scala
+++ src/main/scala/chars/random/package.scala
@@ -4,4 +4,7 @@ package object random {
 
   type Random[T] = (Long) => (Long, T)
 
+  implicit class RandomOps[T](val r: Random[T]) extends AnyVal {
+    def get(seed: Long): T = r.apply(seed)._2
+  }
 }
