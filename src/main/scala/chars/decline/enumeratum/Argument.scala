bd0ff4b0b07d286d520f6c4f9ffd8a881d37c489
Raphael Bosshard <raphael.bosshard@gmail.com>
2018-11-30 22:22:36 +0100

    add implementation

--- src/main/scala/chars/decline/enumeratum/Argument.scala
+++ src/main/scala/chars/decline/enumeratum/Argument.scala
@@ -1,5 +1,18 @@
 package chars.decline.enumeratum
 
-class Argument {
+import cats.data.ValidatedNel
+import com.monovore.decline.Argument
+import enumeratum.{Enum, EnumEntry}
+import cats.implicits._
 
+object Argument {
+
+  def enumArgument[E <: EnumEntry](e: Enum[E]): Argument[E] = new Argument[E] {
+    override def read(string: String): ValidatedNel[String, E] =
+      e.
+        withNameInsensitiveOption(string)
+        .toValidNel(s"unexpected value for ${e.toString}: valid values are: ${e.values.map(_.toString.toLowerCase).mkString(", ")}")
+
+    override def defaultMetavar: String = e.toString
+  }
 }
