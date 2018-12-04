54b0624372d954ec887d3b42f3dec2ccd50553f5
Raphael Bosshard <raphael.bosshard@gmail.com>
2018-12-04 18:38:27 +0100

    initial tit-for-tat and game

--- src/main/scala/chars/app/TitForTat.scala
+++ src/main/scala/chars/app/TitForTat.scala
@@ -1,5 +1,59 @@
 package chars.app
 
+import chars.random.{Generator, Random}
+import chars.cats.random.monad
+import cats.implicits._
+import chars.random.Generator.oneOf
+import chars.titfortat.Game
+import chars.titfortat.Game._
+
 object TitForTat extends {
-  //mvp: run IPD with participants type distribution taken from cmd line
+  //mvp:
+  // run IPD with participants type distribution taken from cmd line
+  //
+
+  val titForTat = new Strategy {
+    def chose(opponentLastAction: Action): Action = opponentLastAction
+  }
+
+  val greedy = new Strategy {
+    def chose(opponentLastAction: Action): Action = Action.Defect
+  }
+
+  val naive = new Strategy {
+    def chose(opponentLastAction: Action): Action = Action.Cooperate
+  }
+
+  val distribution =
+    Map(
+      10 -> titForTat,
+      20 -> greedy,
+      100 -> naive
+    )
+
+  val randomPlayers: Random[Seq[Player]] = Game.randomPlayers(distribution)
+
+  val pairings = randomPlayers.flatMap(buildPairings)
+
+  def buildPairings(players: Seq[Player]): Random[Seq[(PlayerId, PlayerId)]] = { seed: Long =>
+
+    val ids = players.map(_._1)
+
+    ids.foldLeft((seed, Seq.empty[(PlayerId, PlayerId)])) { case ((seed, pairings), curr) =>
+
+      val (newSeed, opponent) = oneOf(ids.filterNot(_ == curr):_*).apply(seed)
+
+      (newSeed, pairings :+ (curr, opponent))
+    }
+  }
+
+  val state =
+    State(
+      score = Map.empty,
+      mostRecentActions = Map.empty
+    )
+
+
+
+
 }
--- src/main/scala/chars/titfortat/Game.scala
+++ src/main/scala/chars/titfortat/Game.scala
@@ -1,11 +1,16 @@
 package chars.titfortat
 
-import chars.random.Generator
-import chars.cats.random._
+import chars.random.{Generator, Random}
+import cats.implicits._
+import chars.cats.random.monad
 import io.estatico.newtype.macros.newtype
 
 object Game {
+  type Payoff = Double
   @newtype case class PlayerId(id: Double)
+  case class Outcome(player: PlayerId, opponent: PlayerId, action: Action, payoff: Payoff)
+  type Pairing = (Player, Player)
+  type Player = (PlayerId, Strategy)
 
   trait Action
 
@@ -14,43 +19,81 @@ object Game {
     case object Defect extends Action
   }
 
-  case class State(score: Map[PlayerId, Double], mostRecentActions: Map[(Player, Player), Action])
-
-  trait Player {
-    def chose(opponentLastAction: Action): Action
+  type InteractionEntryKey = (Set[PlayerId], PlayerId)
+  type InteractionEntry = (InteractionEntryKey, Action)
+  implicit class HistoryOps(val history: Set[InteractionEntry]) extends AnyVal {
+    def findLastAction(key: InteractionEntryKey): Option[InteractionEntry] = {
+      history.toList.reverse.find(_._1 == key)
+    }
   }
 
-  trait Context
+  case class State(score: Map[PlayerId, Double], mostRecentActions: Set[InteractionEntry])
 
-  val participants = {
+  trait Strategy {
+    def chose(state: State, self: Player, other: PlayerId): Action
+  }
+
+
+  def randomPlayers(distribution: Map[Int, Strategy]): Random[Seq[Player]] = { seed: Long =>
 
     val newId = Generator.randomDouble.map(PlayerId)
 
-    val titForTat = new Player {
-      override def chose(opponentLastAction: Action): Action = opponentLastAction
+    val strategies = distribution.flatMap { case (number, strategy) => List.fill(number)(strategy) }
+
+    val init: (Long, Seq[(PlayerId, Strategy)]) = (seed, Seq.empty[(PlayerId, Strategy)])
+
+    strategies.foldLeft(init){ case ((seed, participants), curr: Strategy) =>
+      val (newSeed, id) = newId(seed)
+      (newSeed, participants :+ (id, curr))
     }
-
-    val greedy = new Player {
-      override def chose(opponentLastAction: Action): Action = Action.Defect
-    }
-
-    val naive = new Player {
-      override def chose(opponentLastAction: Action): Action = Action.Cooperate
-    }
-
-    val distribution =
-      Map(
-        titForTat -> 10,
-        greedy -> 20,
-        naive -> 100
-      )
-
-    distribution.map { case (generator) }
   }
 
 
+  type Payoffs = Map[(Action, Action), (Double, Double)]
+
+  def evaluate(payoffs: Payoffs, state: State)(left: Player, right: Player): (Outcome, Outcome) = {
+
+    def action(self: Player, other: PlayerId): Action = {
+      self._2.chose(state, self, other)
+    }
+
+    val leftAction = action(left, right._1)
+    val rightAction = action(right, left._1)
+
+    val (leftPayoff, rightPayoff) = payoffs((leftAction, rightAction))
+
+    (
+      Outcome(left._1, right._1, leftAction, leftPayoff),
+      Outcome(right._1, left._1, rightAction, rightPayoff)
+    )
+  }
+
+  def runPairings(payoffs: Payoffs, state: State, pairings: Set[Pairing]): State = {
+
+    val outcomes =
+      pairings
+        .toList
+        .flatMap { case (left, right) =>
+          val (lOutcome, rOutcome) = evaluate(payoffs, state)(left, right)
+          List(lOutcome, rOutcome)
+        }
 
 
-  val participants =
+    val newScore = outcomes.foldLeft(state.score) { case (acc, Outcome(playerId, opponent, action, payoff)) =>
+      val oldScore = acc.getOrElse(playerId, 0)
+
+      acc + (playerId -> (oldScore + payoff))
+    }
+
+    val mostRecentActions =
+      outcomes.map { case Outcome(playerId, opponent, action, payoff) =>
+        (Set(playerId, opponent), playerId) -> action
+      }.toSet
+
+    state.copy(
+      score = newScore,
+      mostRecentActions = mostRecentActions
+    )
+  }
 
 }
