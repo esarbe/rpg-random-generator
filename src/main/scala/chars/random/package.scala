e170b0526bcd9fabbb8df9014c88863ccac43ca3
Raphael Bosshard <raphael.bosshard@gmail.com>
2018-12-05 20:56:41 +0100

    simplify type signature

--- src/main/scala/chars/random/package.scala
+++ src/main/scala/chars/random/package.scala
@@ -2,7 +2,7 @@ package chars
 
 package object random {
 
-  type Random[T] = (Long) => (Long, T)
+  type Random[T] = Long => (Long, T)
 
   implicit class RandomOps[T](val r: Random[T]) extends AnyVal {
     def get(seed: Long): T = r.apply(seed)._2
